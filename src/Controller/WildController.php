e2be2785f443063c0e01bfc1d942e522be69a2d2
francoisvaillant <frvaillant@gmail.com>
2020-05-08 06:40:31 +0200

    made a first controller

---
+++ src/Controller/WildController.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class WildController extends AbstractController
+{
+    /**
+     * @return Response
+     */
+    /**
+     * @Route("/wild", name="wild_index")
+     */
+    public function index() :Response
+    {
+        return $this->render('wild/index.html.twig', [
+            'website' => 'Wild Séries',
+        ]);
+    }
+
+}
